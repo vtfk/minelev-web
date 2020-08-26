a4b8790010edd29cf0311e2b2aeb6f0748cebbeb
Mats Andreassen <matsand@live.no>
2020-08-26 18:33:23 +0200

    Update protectedRoute to check whether we are authenticated or not

--- src/config/ProtectedRoute.js
+++ src/config/ProtectedRoute.js
@@ -1,15 +1,17 @@
-import React from "react"
-import { Route, Redirect } from "react-router-dom"
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
 
+import { isAuthenticated } from '../lib/is-authenticated'
 import { ROUTES } from '../config/constants'
 
-export function ProtectedRoute(props) {
-  // TODO: auth check
-  const isAuthenticated = true
+export function ProtectedRoute (props) {
+  // Redirect to current page, if not on frontpage
+  const currentPage = window.location.pathname
+  const redirectToParam = currentPage !== '/' ? `?redirect=${currentPage}` : ''
 
-  return isAuthenticated ? (
-      <Route { ...props } />
+  return isAuthenticated() ? (
+    <Route {...props} />
   ) : (
-      <Redirect to={{ pathname: `/${ ROUTES.login }` }} />
+    <Redirect to={`/${ROUTES.login}${redirectToParam}`} />
   )
 }
