a27cec2545a37030d8a1551908f09b6a9e46cb4e
Kristoffer Sundmyhr <kristoffer@sundmyhr.no>
2020-11-19 11:12:00 +0100

    Topnav implementation for mobile version

--- src/layouts/Default/index.js
+++ src/layouts/Default/index.js
@@ -2,10 +2,13 @@ import React, { useState } from 'react'
 import { useSession } from '../../lib/auth-provider'
 
 import {
-  useLocation
+  useLocation,
+  Link
 } from 'react-router-dom'
 
 import { ROUTES } from '../../config/constants'
+
+import ScrollLock from 'react-scrolllock';
 
 import { SideNav, SideNavItem } from '../../_lib-components/SideNav'
 import { SearchField } from '../../_lib-components/SearchField'
@@ -13,6 +16,7 @@ import { InitialsBadge } from '../../_lib-components/InitialsBadge'
 import { Paragraph } from '../../_lib-components/Typography'
 import { IconDropdownNav, IconDropdownNavItem } from '../../_lib-components/IconDropdownNav'
 import { Icon } from '../../_lib-components/Icon'
+import { Logo } from '../../_lib-components/Logo'
 
 import './styles.scss'
 
@@ -20,50 +24,149 @@ export function DefaultLayout (props) {
   const { user, logout } = useSession()
   const location = useLocation()
   const [searchTerm, setSearchTerm] = useState(new URLSearchParams(location.search).get('s'))
+  const [openTopNavSide, setOpenTopNavSide] = useState(false)
+  const [scrollLock, setScrollLock] = useState(false)
+
+  function clickTopNavToggle() {
+    var newIsOpen = !openTopNavSide
+    setOpenTopNavSide(newIsOpen)
+
+    if (newIsOpen && window.innerWidth <= 1000) {
+      setScrollLock(true)
+    } else {
+      setScrollLock(false)
+    }
+  }
 
   return (
-    <div className='default-layout'>
-      <SideNav title='MinElev'>
-        <SideNavItem icon={<Icon name='home' />} active={location.pathname === '/'} href='/' title='Forside' />
-        <SideNavItem icon={<Icon name='activity' />} active={location.pathname === `/${ROUTES.activityLog}`} href={`/${ROUTES.activityLog}`} title='Aktivitetslogg' />
-        <SideNavItem icon={<Icon name='students' />} active={location.pathname === `/${ROUTES.students}`} href={`/${ROUTES.students}`} title='Elever' />
-        <SideNavItem icon={<Icon name='classes' />} active={location.pathname === `/${ROUTES.classes}`} href={`/${ROUTES.classes}`} title='Klasser' />
-        <SideNavItem icon={<Icon name='statistics' />} active={location.pathname === `/${ROUTES.statistics}`} href={`/${ROUTES.statistics}`} title='Statistikk' />
-        <SideNavItem icon={<Icon name='help' />} active={location.pathname === `/${ROUTES.help}`} href={`/${ROUTES.help}`} title='Hjelp' />
-      </SideNav>
+    <ScrollLock isActive={scrollLock}>
+      <div className='default-layout'>
+        <SideNav title='MinElev'>
+          <SideNavItem icon={<Icon name='home' />} active={location.pathname === '/'} href='/' title='Forside' />
+          <SideNavItem icon={<Icon name='activity' />} active={location.pathname === `/${ROUTES.activityLog}`} href={`/${ROUTES.activityLog}`} title='Aktivitetslogg' />
+          <SideNavItem icon={<Icon name='students' />} active={location.pathname === `/${ROUTES.students}`} href={`/${ROUTES.students}`} title='Elever' />
+          <SideNavItem icon={<Icon name='classes' />} active={location.pathname === `/${ROUTES.classes}`} href={`/${ROUTES.classes}`} title='Klasser' />
+          <SideNavItem icon={<Icon name='statistics' />} active={location.pathname === `/${ROUTES.statistics}`} href={`/${ROUTES.statistics}`} title='Statistikk' />
+          <SideNavItem icon={<Icon name='help' />} active={location.pathname === `/${ROUTES.help}`} href={`/${ROUTES.help}`} title='Hjelp' />
+        </SideNav>
 
-      <div className='container'>
-        <div className='action-bar'>
-          <div className='search'>
-            <SearchField
-              className='search-input'
-              type='text' placeholder='Søk etter elev ...'
-              onChange={(event) => { setSearchTerm(event.target.value) }}
-              value={searchTerm || ''}
-              onKeyPress={event => {
-                if (event.key === 'Enter') {
-                  window.location.replace(`/${ROUTES.students}?s=${event.target.value}`)
-                }
-              }}
-            />
+        { /* TODO: To lib component when desktop design is in place */ }
+        <div className={`topnav-side ${openTopNavSide ? 'open' : ''}`}>
+          <div className="topnav-side-top">
+            <div className="search">
+              <SearchField
+                className='search-input'
+                type='text' placeholder='Søk etter elev ...'
+                onChange={(event) => { setSearchTerm(event.target.value) }}
+                value={searchTerm || ''}
+                onKeyPress={event => {
+                  if (event.key === 'Enter') {
+                    window.location.replace(`/${ROUTES.students}?s=${event.target.value}`)
+                  }
+                }}
+              />
+            </div>
+
+            <Link className="topnav-side-top-close" onClick={clickTopNavToggle}>
+              <Icon name='close' size="xsmall" />
+            </Link>
           </div>
 
-          <div className='user'>
-            <div className='user-name'>
-              <Paragraph>{user.displayName}</Paragraph>
+          <div className="topnav-side-list">
+            <div className="topnav-side-list-inner">
+              <Link className="topnav-side-list-item" to={'/'}>
+                <div className="topnav-side-list-item-icon"><Icon size="medium" name='home' /></div>
+                <div className="topnav-side-list-item-text">Forside</div>
+              </Link>
+              <Link className="topnav-side-list-item" to={`/${ROUTES.activityLog}`}>
+                <div className="topnav-side-list-item-icon"><Icon size="medium" name='activity' /></div>
+                <div className="topnav-side-list-item-text">Aktivitetslogg</div>
+              </Link>
+              <Link className="topnav-side-list-item" to={`/${ROUTES.students}`}>
+                <div className="topnav-side-list-item-icon"><Icon size="medium" name='students' /></div>
+                <div className="topnav-side-list-item-text">Elever</div>
+              </Link>
+              <Link className="topnav-side-list-item" to={`/${ROUTES.classes}`}>
+                <div className="topnav-side-list-item-icon"><Icon size="medium" name='classes' /></div>
+                <div className="topnav-side-list-item-text">Klasser</div>
+              </Link>
+              <Link className="topnav-side-list-item" to={`/${ROUTES.statistics}`}>
+                <div className="topnav-side-list-item-icon"><Icon size="medium" name='statistics' /></div>
+                <div className="topnav-side-list-item-text">Statistikk</div>
+              </Link>
+              <Link className="topnav-side-list-item" to={`/${ROUTES.help}`}>
+                <div className="topnav-side-list-item-icon"><Icon size="medium" name='help' /></div>
+                <div className="topnav-side-list-item-text">Hjelp</div>
+              </Link>
             </div>
-            <InitialsBadge className='user-image' firstName={user.givenName} lastName={user.surname} />
-            <div className='user-menu'>
-              <IconDropdownNav>
-                <IconDropdownNavItem onClick={() => { window.alert('Ikke implementert') }} title='Min konto' />
-                <IconDropdownNavItem onClick={() => logout()} title='Logg ut' />
-              </IconDropdownNav>
+          </div>
+
+          <div className="topnav-side-bottom">
+            <div className='user'>
+              <InitialsBadge className='user-image' firstName={user.givenName} lastName={user.surname} />
+              <div className='user-name'>
+                <Paragraph>{user.displayName}</Paragraph>
+              </div>
+              <div className='user-menu'>
+                <IconDropdownNav>
+                  <IconDropdownNavItem onClick={() => { window.alert('Ikke implementert') }} title='Min konto' />
+                  <IconDropdownNavItem onClick={() => logout()} title='Logg ut' />
+                </IconDropdownNav>
+              </div>
             </div>
           </div>
         </div>
 
-        {props.children}
+        <div className='container'>
+          <div className="topnav">
+            <div className='topnav-brand'>
+              <div className='brand-logo'>
+                <a href='/'>
+                  <Logo />
+                </a>
+              </div>
+              <div className='brand-name'>
+                MinElev
+              </div>
+            </div>
+            <div className="topnav-toggles" onClick={clickTopNavToggle}>
+              <Icon size="small" name='search' />
+              <Icon size="small" name='menu' />
+            </div>
+          </div>
+
+          <div className='action-bar'>
+            <div className='search'>
+              <SearchField
+                className='search-input'
+                type='text' placeholder='Søk etter elev ...'
+                onChange={(event) => { setSearchTerm(event.target.value) }}
+                value={searchTerm || ''}
+                onKeyPress={event => {
+                  if (event.key === 'Enter') {
+                    window.location.replace(`/${ROUTES.students}?s=${event.target.value}`)
+                  }
+                }}
+              />
+            </div>
+
+            <div className='user'>
+              <div className='user-name'>
+                <Paragraph>{user.displayName}</Paragraph>
+              </div>
+              <InitialsBadge className='user-image' firstName={user.givenName} lastName={user.surname} />
+              <div className='user-menu'>
+                <IconDropdownNav>
+                  <IconDropdownNavItem onClick={() => { window.alert('Ikke implementert') }} title='Min konto' />
+                  <IconDropdownNavItem onClick={() => logout()} title='Logg ut' />
+                </IconDropdownNav>
+              </div>
+            </div>
+          </div>
+
+          {props.children}
+        </div>
       </div>
-    </div>
+    </ScrollLock>
   )
 }
