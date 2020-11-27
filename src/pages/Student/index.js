42b39f2e93b502ce21b1a7c81cf8e84b1b48633f
Kristoffer Sundmyhr <kristoffer@sundmyhr.no>
2020-11-27 07:40:19 +0100

    Cleanup

--- src/pages/Student/index.js
+++ src/pages/Student/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Fragment } from 'react'
+import React, { useState, useEffect } from 'react'
 import Moment from 'react-moment'
 
 import { DefaultLayout } from '../../layouts/Default'
