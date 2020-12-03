457a70e50760771373abef53faf1b214dc31b041
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-12-03 20:48:57 +0100

    Ferdigstiller utdanningsprogrammer-selector-form

--- src/components/utdanningsprogrammer-selector-form.js
+++ src/components/utdanningsprogrammer-selector-form.js
@@ -1,11 +1,54 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSession } from '@vtfk/react-msal'
 import useGrep from '../hooks/use-grep'
 import { Select } from '../_lib-components/Select'
 
-function Utdanningsprogrammer ({ data }) {
+function Programomrader ({ data, trinn }) {
   const [select, setSelect] = useState('')
-  console.log(data)
+  const [filteredData, setFilteredData] = useState(false)
+
+  useEffect(() => {
+    if (trinn && data) {
+      const omrade = data[0].data
+      const trinnData = omrade.trinn.find(item => item.kode === trinn.value)
+      if (trinnData) {
+        setFilteredData(trinnData.programomraader)
+      }
+    }
+  }, [trinn, data])
+
+  if (!filteredData) {
+    return null
+  }
+  const items = filteredData.map(item => {
+    const { kode, tittel } = item
+    return {
+      value: kode,
+      label: tittel.nb
+    }
+  })
+  return (
+    <div className='input-element'>
+      <Select
+        placeholder='Velg programområde'
+        items={items}
+        selectedItem={select}
+        onChange={(item) => { setSelect(item) }}
+        isOpen
+        closeOnSelect
+      />
+    </div>
+  )
+}
+
+function Utdanningsprogrammer ({ data, setQuery }) {
+  const [select, setSelect] = useState('')
+
+  useEffect(() => {
+    if (select !== '') {
+      setQuery(select.value)
+    }
+  }, [select])
   if (!data) {
     return null
   }
@@ -33,7 +76,16 @@ function Utdanningsprogrammer ({ data }) {
 function UtdanningsprogrammerSelectorForm () {
   const [select, setSelect] = useState()
   const { apiGet } = useSession()
-  const { data } = useGrep(apiGet)
+  const {
+    utdanningsprogrammer,
+    programomrader,
+    setQuery
+  } = useGrep(apiGet)
+
+  useEffect(() => {
+    setQuery('')
+  }, [])
+
   return (
     <>
       <div className='input-element'>
@@ -47,9 +99,11 @@ function UtdanningsprogrammerSelectorForm () {
           selectedItem={select}
           onChange={(item) => { setSelect(item) }}
           closeOnSelect
+          isOpen
         />
       </div>
-      {data && select && <Utdanningsprogrammer data={data.data} />}
+      {utdanningsprogrammer && select && <Utdanningsprogrammer data={utdanningsprogrammer.data} setQuery={setQuery} />}
+      {programomrader && select && <Programomrader data={programomrader.data} trinn={select} />}
     </>
   )
 }
