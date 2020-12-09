de25586329f44716192c69f9277bd5a4ce22f33f
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-12-09 18:51:06 +0100

    Legger til rammen for vurdering av kompetansemål

--- src/containers/YffReviewModal/index.js
+++ src/containers/YffReviewModal/index.js
@@ -11,6 +11,8 @@ import { InitialsBadge } from '../../_lib-components/InitialsBadge'
 import { Modal, ModalBody, ModalSideActions } from '../../_lib-components/Modal'
 import { TextField } from '../../_lib-components/TextField'
 import Evaluation from './evaluation'
+import Review from './review'
+import maal from '../../mocks/laereplan'
 
 import './styles.scss'
 
@@ -79,60 +81,8 @@ export function YffReviewModal ({ selectedStudentId, ...props }) {
           </p>
 
           <div className='form'>
-
-            <h2 className='subheader'>Tilbakemelding</h2>
-
-            <table className='data-actions-table'>
-              <thead>
-                <tr>
-                  <th><Paragraph size='small'>Kompetansemål / Arbeidsoppgaver</Paragraph></th>
-                  <th><Paragraph size='small'>Utplasseringssted</Paragraph></th>
-                  <th className='actions-th'><Paragraph size='small'>Ny handling</Paragraph></th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>
-                    <Paragraph>
-                      Utarbeide framdriftsplanar for arbeidet og bestille materiale i samsvar med teikningar og beskrivingar
-                    </Paragraph>
-                  </td>
-                  <td>
-                    Vest-Telemark vidaregåande skule, avdeling Seljord
-                  </td>
-                  <td className='actions'>
-                    <Link onClick={() => { window.alert('Ikke implementert') }}>Fjern</Link>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <Paragraph>
-                      Bruke digitale måleverktøy
-                    </Paragraph>
-                  </td>
-                  <td>
-                    Vest-Telemark vidaregåande skule, avdeling Seljord
-                  </td>
-                  <td className='actions'>
-                    <Link onClick={() => { window.alert('Ikke implementert') }}>Fjern</Link>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <Paragraph>
-                      Bruke nivellerings- og oppmålingsutstyr til stikking av anleggstekniske arbeid
-                    </Paragraph>
-                  </td>
-                  <td>
-                    Vest-Telemark vidaregåande skule, avdeling Seljord
-                  </td>
-                  <td className='actions'>
-                    <Link onClick={() => { window.alert('Ikke implementert') }}>Fjern</Link>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
             <form id='review-form'>
+              <Review maal={maal} />
               <Evaluation />
               <h2 className='subheader'>Fravær under utplasseringen</h2>
               <div className='input-element'>
