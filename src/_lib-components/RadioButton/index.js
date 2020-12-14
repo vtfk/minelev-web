f4fe93eb110fb4f09edd9dd417182163ebd723e4
Kristoffer Sundmyhr <kristoffer@sundmyhr.no>
2020-12-15 00:02:19 +0100

    Standard fix

--- src/_lib-components/RadioButton/index.js
+++ src/_lib-components/RadioButton/index.js
@@ -1,15 +1,15 @@
-import React, { useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 import './styles.scss'
 
 export function RadioButton ({ name, value, onChange, label, ...props }) {
   return (
-    <div className={`radio-button`} {...props}>
-      <div className="radio-button-inner">
-        <div className="radio-wrapper">
-          <input id={`radio-${name}-${value}`} type="radio" name={name} value={value} onChange={(e) => { onChange(e.target.value) }} />
-          <div className="radio"></div>
+    <div className='radio-button' {...props}>
+      <div className='radio-button-inner'>
+        <div className='radio-wrapper'>
+          <input id={`radio-${name}-${value}`} type='radio' name={name} value={value} onChange={(e) => { onChange(e.target.value) }} />
+          <div className='radio' />
         </div>
         <label for={`radio-${name}-${value}`}>{label}</label>
       </div>
@@ -21,5 +21,5 @@ RadioButton.propTypes = {
   name: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
-  label: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired
 }
