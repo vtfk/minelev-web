27004359520f45e13dc8b6d92e5c3c7a3acdaf63
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-12-15 18:01:48 +0100

    Fjerner onChange som required

--- src/_lib-components/RadioButton/index.js
+++ src/_lib-components/RadioButton/index.js
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types'
 
 import './styles.scss'
 
-export function RadioButton ({ name, value, onChange, label, ...props }) {
+export function RadioButton ({ name, value, label, ...props }) {
   return (
     <div className='radio-button' {...props}>
       <div className='radio-button-inner'>
         <div className='radio-wrapper'>
-          <input id={`radio-${name}-${value}`} type='radio' name={name} value={value} onChange={(e) => { onChange(e.target.value) }} />
+          <input id={`radio-${name}-${value}`} type='radio' name={name} value={value} {...props} />
           <div className='radio' />
         </div>
         <label htmlFor={`radio-${name}-${value}`}>{label}</label>
