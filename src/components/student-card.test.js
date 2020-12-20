9643123ccd7a646bf17e0d487246894620f89cc8
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-12-20 09:54:21 +0100

    Nytt forsøk på mindre flaky test

--- src/components/student-card.test.js
+++ src/components/student-card.test.js
@@ -7,14 +7,14 @@ describe('tester StudentCard komponenten', () => {
     const { container } = render(<StudentCard />)
     expect(container).toBeEmptyDOMElement()
   })
-  test('komponenten rendres som forventet', () => {
+  test('komponenten rendres som forventet', async () => {
     const student = STUDENTS[0]
     render(<StudentCard student={student} />)
     expect(screen.getByText(/alvorlig/i)).toBeInTheDocument()
     expect(screen.getByText(/lykke/i)).toBeInTheDocument()
     expect(screen.getByText(/testen videregående skole/i)).toBeInTheDocument()
     expect(screen.getByText(/TESVS:2STB/i)).toBeInTheDocument()
-    expect(screen.getByText(/17. juni 2004/i) || screen.getByText(/June 17, 2004/i)).toBeInTheDocument()
+    expect(await screen.queryByText(/17. juni 2004/i) || await screen.queryByText(/June 17, 2004/i)).toBeInTheDocument()
     expect(screen.getByText(/alv1706@skole.vtfk.no/i)).toBeInTheDocument()
   })
 })
