06d979ebe350f288f800bf269e797759d4459f06
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-12-20 07:36:25 +0100

    Oppdaterer company-details

--- src/containers/YffConfirmationModal/company-details.js
+++ src/containers/YffConfirmationModal/company-details.js
@@ -1,16 +1,50 @@
 // TODO: Vise hentet info
+
+function repackCompany (company) {
+  const {
+    navn: bedriftsNavn,
+    orgnr: organisasjonsNummer,
+    postadresse: adresse,
+    ppoststed: poststed,
+    ppostnr: postnummer,
+    forretningsadr: forretningsadresse,
+    forradrpostnr: forretningspostnummer,
+    forradrpoststed: forretningspoststed
+  } = company
+
+  return {
+    organisasjonsNummer,
+    navn: bedriftsNavn,
+    adresse: adresse || forretningsadresse,
+    postnummer: postnummer || forretningspostnummer,
+    poststed: poststed || forretningspoststed
+  }
+}
+
 const CompanyDetails = props => {
   const { company } = props
   const kanViseKomponent = company
-
   if (!kanViseKomponent) {
     return null
   }
 
+  const {
+    organisasjonsNummer,
+    navn,
+    adresse,
+    postnummer,
+    poststed
+  } = repackCompany(company)
+
   return (
     <div className='prefilled'>
-      <div className='prefilled-label'>{company.navn}</div>
-      <div className='prefilled-text'>Hentet info her</div>
+      <div className='prefilled-label'>{navn}</div>
+      <div className='prefilled-text'>
+        <strong>Organisasjonsnummer:</strong> {organisasjonsNummer}<br />
+        <strong>Adresse:</strong> {adresse}<br />
+        <strong>Postnummer:</strong> {postnummer}<br />
+        <strong>Poststed:</strong> {poststed}<br />
+      </div>
     </div>
   )
 }
