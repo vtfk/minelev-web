e4c89b4146dd4b036d4efe41f495412b85c22458
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-12-20 08:33:31 +0100

    Bruker StudentCard

--- src/containers/YffReviewModal/index.js
+++ src/containers/YffReviewModal/index.js
@@ -4,14 +4,13 @@ import PropTypes from 'prop-types'
 
 import { useSession } from '@vtfk/react-msal'
 
-import { ROUTES } from '../../config/constants'
 import { API } from '../../config/app'
 
-import { Heading3, Paragraph, Link } from '../../_lib-components/Typography'
+import { Heading3, Link } from '../../_lib-components/Typography'
 import { RadioButton } from '../../_lib-components/RadioButton'
-import { InitialsBadge } from '../../_lib-components/InitialsBadge'
 import { Modal, ModalBody, ModalSideActions } from '../../_lib-components/Modal'
 import { TextField } from '../../_lib-components/TextField'
+import StudentCard from '../../components/student-card'
 import Evaluation from './evaluation'
 import Review from './review'
 import Attitude from './attitude'
@@ -121,22 +120,7 @@ export function YffReviewModal ({ selectedStudentId, utplasseringsId, ...props }
           {
             selectedStudent &&
             selectedStudent.firstName &&
-              <div className='person-information'>
-                <div className='image'>
-                  <InitialsBadge firstName={selectedStudent.firstName} lastName={selectedStudent.lastName} size='large' />
-                </div>
-                <div className='text-wrapper'>
-                  <Heading3 className='name'>
-                    {selectedStudent.firstName} {selectedStudent.lastName}
-                  </Heading3>
-                  <div className='other'>
-                    <Paragraph>{selectedStudent.schoolName}</Paragraph>
-                    <Paragraph><Link href={`/${ROUTES.classes}/${selectedStudent.classId}`}>{selectedStudent.className}</Link></Paragraph>
-                    <Paragraph>26. april 2001</Paragraph>
-                    <Paragraph>bra26041@skole.vtfk.no</Paragraph>
-                  </div>
-                </div>
-              </div>
+              <StudentCard student={selectedStudent} />
           }
           <Details utplassering={utplassering} />
           <p className='intro'>
