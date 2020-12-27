943094f4cf4219e1f631b9d2e6d2c02df83c5425
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-12-27 08:59:10 +0100

    Oppdaterer tester for kompetanse-selector-form med referanse

--- src/containers/YffCurriculumModal/kompetansemal-selector-form.test.js
+++ src/containers/YffCurriculumModal/kompetansemal-selector-form.test.js
@@ -3,6 +3,10 @@ import KompetansemalSelectorForm from './kompetansemal-selector-form'
 import ba from '../../mocks/ba.json'
 import userEvent from '@testing-library/user-event'
 const kompetansemaal = ba.data.trinn[0].programomraader[0].maal
+const referanse = {
+  referanseID: '123',
+  referanseTittel: 'Fin, fake tittel'
+}
 
 describe('Tester KompetansemalSelectorForm', () => {
   test('komponenten vises IKKE uten å ha mål å vise', () => {
@@ -11,12 +15,12 @@ describe('Tester KompetansemalSelectorForm', () => {
   })
 
   test('komponenten VISES dersom den mottar kompetansemål', () => {
-    render(<KompetansemalSelectorForm kompetansemaal={kompetansemaal} />)
+    render(<KompetansemalSelectorForm kompetansemaal={kompetansemaal} referanse={referanse} />)
     expect(screen.getByText(/velg kompetansemål/i)).toBeInTheDocument()
   })
 
   test('komponenten ekspanderes dersom man klikker', () => {
-    render(<KompetansemalSelectorForm kompetansemaal={kompetansemaal} />)
+    render(<KompetansemalSelectorForm kompetansemaal={kompetansemaal} referanse={referanse} />)
     expect(screen.getByText(/velg kompetansemål/i)).toBeInTheDocument()
     userEvent.click(screen.getByText(/velg kompetansemål/i))
     expect(screen.getByText(/foreta masseberegninger/i)).toBeInTheDocument()
@@ -24,7 +28,7 @@ describe('Tester KompetansemalSelectorForm', () => {
   })
 
   test('man kan klikke på items for å velge de og avvelge', async () => {
-    render(<KompetansemalSelectorForm kompetansemaal={kompetansemaal} />)
+    render(<KompetansemalSelectorForm kompetansemaal={kompetansemaal} referanse={referanse} />)
     userEvent.click(screen.getByText(/velg kompetansemål/i))
     expect(screen.getByText(/foreta masseberegninger/i)).toBeInTheDocument()
     const knapp = screen.getByText(/foreta masseberegninger/i)
@@ -35,7 +39,7 @@ describe('Tester KompetansemalSelectorForm', () => {
   })
 
   test('man velger mål og arbeidsoppgaver dukker opp', async () => {
-    render(<KompetansemalSelectorForm kompetansemaal={kompetansemaal} />)
+    render(<KompetansemalSelectorForm kompetansemaal={kompetansemaal} referanse={referanse} />)
     userEvent.click(screen.getByText(/velg kompetansemål/i))
     expect(screen.getByText(/foreta masseberegninger/i)).toBeInTheDocument()
     expect(await screen.queryByText(/beskriv arbeidsoppgaver/i)).not.toBeInTheDocument()
