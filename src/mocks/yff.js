ae3c105476e52054f7222f379de2b0414e109bba
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-12-29 08:16:00 +0100

    Fjerner objekter som ikke lenger finnes i modellen

--- src/mocks/yff.js
+++ src/mocks/yff.js
@@ -1,10 +1,8 @@
 /**
  * YFF består egentlig bare av mål og utplassering
- * læreplan = alle mål
- * tilbakemelding = utplassering og mål/arbeidsoppgaver lagt opp for den
+ * læreplan = alle mål (knyttet mot utplasseringer)
+ * tilbakemelding = utplassering og mål/arbeidsoppgaver lagt opp for utplasseringen
  */
-
-const laereplan = require('./laereplan')
 
 const maal = {
   _id: '5d7f2af09f7cda0008d292d4',
@@ -103,58 +101,6 @@ const utplassering = {
 }
 
 const tilbakemelding = {
-  _id: '5d8b4b7f429a8900087e6379',
-  type: 'tilbakemelding',
-  created: {
-    timeStamp: 1568615152589,
-    createdBy: 'arv1204'
-  },
-  modified: [
-    {
-      timeStamp: 1568615152589,
-      modifiedBy: 'arv1204'
-    }
-  ],
-  studentUserName: 'fer0804',
-  utplasseringID: '5d7f29d059e31d0007cc9ff5',
-  maalVurderinger: [
-    {
-      id: '5d7f2af3d533e7000892fa6c',
-      name: 'reflektere over egen praksis',
-      description: '',
-      score: 'Høy måloppnåelse'
-    },
-    {
-      id: '5d7f2af368b92400087c28e5',
-      name: 'delta i tverrfaglig samarbeid',
-      description: '',
-      score: 'Middels måloppnåelse'
-    },
-    {
-      id: '5d7f2af0190c8b0007843d0d',
-      name: 'følge gjeldende regelverk for taushetsplikt og personvern i helse- og sosialsektoren',
-      description: '',
-      score: 'Høy måloppnåelse'
-    },
-    {
-      id: '5d7f2af08e4c4c0007c067b2',
-      name: 'identifisere underernæring og feilernæring og foreslå tiltak for å forebygge og behandle disse',
-      description: '',
-      score: 'Middels måloppnåelse'
-    },
-    {
-      id: '5d7f2af059e31d0007cc9ff6',
-      name: 'utføre arbeidet i tråd med miljøterapeutiske målsettinger',
-      description: '',
-      score: 'Middels måloppnåelse'
-    },
-    {
-      id: '5d7f2af09f7cda0008d292d4',
-      name: 'foreslå og sette i verk tiltak som fremmer mestring, helse og trivsel, og som stimulerer til et aktivt liv ',
-      description: '',
-      score: 'Middels måloppnåelse'
-    }
-  ],
   evaluering: [
     {
       id: 'hms',
@@ -219,16 +165,12 @@ module.exports = params => {
   const data = {
     maal,
     maalmal,
-    utplassering,
-    tilbakemelding,
-    laereplan
+    utplassering
   }
   if (student && !type && !id) {
     return {
       maal: arr.fill(maal),
-      utplasseringer: [utplassering],
-      tilbakemeldinger: [tilbakemelding],
-      laereplan: laereplan
+      utplasseringer: [utplassering]
     }
   } else {
     return data[type] ? arr.fill(data[type]) : []
@@ -236,3 +178,5 @@ module.exports = params => {
 }
 
 module.exports.utplassering = utplassering
+
+module.exports.tilbakemelding = tilbakemelding
