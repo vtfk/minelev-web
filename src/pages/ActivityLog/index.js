c228f991afab0ff2182c12b8d80133650d5a86ba
Mats Andreassen <matsand@live.no>
2021-01-02 19:17:15 +0100

    Sett brukernavnet som selectedStudent, så kallene videre går som de skal

--- src/pages/ActivityLog/index.js
+++ src/pages/ActivityLog/index.js
@@ -33,7 +33,7 @@ export function ActivityLog () {
   }
 
   function openNoteModal (document) {
-    setSelectedStudent(document)
+    setSelectedStudent(document.student.username)
     setNoteModalState(true)
   }
 
