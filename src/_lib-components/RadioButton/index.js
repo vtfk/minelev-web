1980eeb0da84c038c0d9582ebfce4280388d563d
Mats Andreassen <matsand@live.no>
2021-01-08 22:48:00 +0100

    La til onChange på radioknapp så ikke endringer trigges dobbelt (fordi overordnet div hadde samme prop satt til seg)

--- src/_lib-components/RadioButton/index.js
+++ src/_lib-components/RadioButton/index.js
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types'
 
 import './styles.scss'
 
-export function RadioButton ({ name, value, label, ...props }) {
+export function RadioButton ({ name, value, label, onChange, ...props }) {
   return (
     <div className='radio-button' {...props}>
       <div className='radio-button-inner'>
         <div className='radio-wrapper'>
-          <input id={`radio-${name}-${value}`} type='radio' name={name} value={value} {...props} />
+          <input id={`radio-${name}-${value}`} type='radio' name={name} value={value} onChange={onChange} {...props} />
           <div className='radio' />
         </div>
         <label htmlFor={`radio-${name}-${value}`}>{label}</label>
