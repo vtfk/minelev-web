a5015f262e5e9032224b4af1151f2001552b31cd
Mats Andreassen <matsand@live.no>
2021-01-18 17:45:20 +0100

    La til VERCEL_DSN og VERCEL_ENV i sentry config

--- src/config/app.js
+++ src/config/app.js
@@ -3,6 +3,6 @@ export const API = {
 }
 
 export const SENTRY = {
-  dsn: process.env.REACT_APP_SENTRY_DSN || false,
-  environment: process.env.REACT_APP_SENTRY_ENV || process.env.REACT_APP_IS_MOCK ? 'demo' : process.env.NODE_ENV
+  dsn: process.env.REACT_APP_SENTRY_DSN || process.env.SENTRY_DSN || false,
+  environment: process.env.REACT_APP_SENTRY_ENV || process.env.VERCEL_ENV || process.env.NODE_ENV || process.env.REACT_APP_IS_MOCK ? 'demo' : 'unknown'
 }
