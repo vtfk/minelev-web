25f0661721ad8aaf55e839d3b336a970bcb9c2b0
Mats Andreassen <matsand@live.no>
2021-01-21 18:38:57 +0100

    La til error state på datovelger

--- src/_lib-components/Datepicker/index.js
+++ src/_lib-components/Datepicker/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 import DatePicker, { registerLocale } from 'react-datepicker'
@@ -8,28 +8,46 @@ import nb from 'date-fns/locale/nb'
 import iconCalendar from './icon-calendar.svg'
 
 import './styles.scss'
+import { nanoid } from 'nanoid'
 
-export function Datepicker ({ placeholder, selected, ...props }) {
+export function Datepicker ({ placeholder, label, selected, id, isOpen, error, ...props }) {
+  const [open, setOpen] = useState(isOpen || false)
+  const [labelId] = useState(id || nanoid())
   registerLocale('nb', nb)
 
   return (
-    <div className='datepicker-field'>
+    <div className={`datepicker-field ${error ? 'error' : ''}`}>
       {
         selected &&
-          <div className='placeholder-label'>{placeholder}</div>
+          <label htmlFor={labelId} className='placeholder-label'>
+            {label || placeholder}
+          </label>
       }
+      <div className='picker'>
+        <DatePicker
+          id={labelId}
+          placeholderText={placeholder || ''}
+          selected={selected}
+          locale='nb'
+          dateFormat='dd.MM.yyyy'
+          aria-invalid={!!error}
+          onFocus={() => setOpen(true)}
+          onClickOutside={() => setOpen(false)}
+          open={open}
+          {...props}
+        />
 
-      <DatePicker
-        placeholderText={placeholder || ''}
-        selected={selected}
-        locale='nb'
-        dateFormat='dd.MM.yyyy'
-        {...props}
-      />
-
-      <div className='icon'>
-        <img src={iconCalendar} alt='' />
+        <div className='icon' onClick={() => setOpen(!open)}>
+          <img src={iconCalendar} alt='' />
+        </div>
       </div>
+
+      {
+        error &&
+          <label htmlFor={labelId} role='alert' aria-live='assertive' className='error-message'>
+            {error.message || error}
+          </label>
+      }
     </div>
   )
 }
