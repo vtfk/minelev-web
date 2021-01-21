914759c18d0305e42ce2fbf8d92df265072db470
Mats Andreassen <matsand@live.no>
2021-01-21 20:08:34 +0100

    Oppdaterte skeleton-komponent til å ha flere varianter. 'text' for å hente høyde fra hvor enn man er, 'circle' for å få en rund loader, eller 'rectangle' for å kunne velge bredde selv.

--- src/_lib-components/SkeletonLoader/index.js
+++ src/_lib-components/SkeletonLoader/index.js
@@ -3,12 +3,23 @@ import PropTypes from 'prop-types'
 
 import './styles.scss'
 
-export function SkeletonLoader ({ size, ...props }) {
+export function SkeletonLoader ({ variant = 'text', width, height, className, style, title, ...props }) {
+  
   return (
-    <div className={`skeleton-loader ${size || ''}`} {...props} />
+    <span 
+      className={`skeleton-loader ${variant || ''} ${props.children ? `with-children ${!height ? 'auto-height' : ''} ${!width ? 'auto-width' : ''}` : ''} ${className || ''}`} 
+      style={{width, height, ...style}} 
+      title={title || 'Laster inn...'}
+      {...props} 
+    />
   )
 }
 
 SkeletonLoader.propTypes = {
-  size: PropTypes.string
+  variant: PropTypes.oneOf(['text', 'circle', 'rectangle']),
+  width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  style: PropTypes.object,
+  title: PropTypes.string,
+  children: PropTypes.any
 }
