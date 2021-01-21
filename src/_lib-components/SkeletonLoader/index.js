cef423c436087f46757bb402870335d83040bcf5
Mats Andreassen <matsand@live.no>
2021-01-21 20:09:49 +0100

    La til 'randomWidth' for få en tilfeldig width (i prosent) mellom to tall. Forventer å få inn en array med format; [minimum, maks].

--- src/_lib-components/SkeletonLoader/index.js
+++ src/_lib-components/SkeletonLoader/index.js
@@ -3,8 +3,13 @@ import PropTypes from 'prop-types'
 
 import './styles.scss'
 
-export function SkeletonLoader ({ variant = 'text', width, height, className, style, title, ...props }) {
-  
+export function SkeletonLoader ({ variant = 'text', width, height, randomWidth, className, style, title, ...props }) {
+
+  if(randomWidth) {
+    const [min, max] = randomWidth
+    width = `${Math.random() * (max - min) + min}%`
+  }
+
   return (
     <span 
       className={`skeleton-loader ${variant || ''} ${props.children ? `with-children ${!height ? 'auto-height' : ''} ${!width ? 'auto-width' : ''}` : ''} ${className || ''}`} 
@@ -19,6 +24,7 @@ SkeletonLoader.propTypes = {
   variant: PropTypes.oneOf(['text', 'circle', 'rectangle']),
   width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
   height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  randomWidth: PropTypes.arrayOf([PropTypes.number, PropTypes.string]),
   style: PropTypes.object,
   title: PropTypes.string,
   children: PropTypes.any
