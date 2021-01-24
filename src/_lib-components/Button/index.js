35e1435250655391d3e9395114c7e20eb7fd5cb3
Mats Andreassen <matsand@live.no>
2021-01-24 13:05:58 +0100

    La til spinner på knapp

--- src/_lib-components/Button/index.js
+++ src/_lib-components/Button/index.js
@@ -3,13 +3,18 @@ import PropTypes from 'prop-types'
 
 import { Link } from '../Typography'
 import { Icon } from '../Icon'
+import { Spinner } from '../Spinner'
 
 import './styles.scss'
 
-export function Button ({ className, type, children, ...props }) {
+export function Button ({ className, type, spinner, disabled, children, ...props }) {
   return (
-    <button className={`button button-${type} ${className}`} {...props}>
-      {children}
+    <button
+      className={`button button-${type || 'primary'} ${className}`}
+      disabled={disabled || spinner || false}
+      {...props}
+    >
+      {spinner ? <Spinner /> : children}
     </button>
   )
 }
@@ -33,6 +38,8 @@ export function IconButtonLink ({ className, type, icon, children, ...props }) {
 Button.propTypes = {
   className: PropTypes.string,
   type: PropTypes.string,
+  disabled: PropTypes.bool,
+  spinner: PropTypes.bool,
   children: PropTypes.string.isRequired
 }
 
