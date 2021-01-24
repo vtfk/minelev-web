54f36f0993d9e52845a9eb51153fac2d2bfd22dc
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-01-24 13:48:42 +0100

    Oppdaterer tester for kopi til e-postmottager

--- src/containers/YffConfirmationModal/company-email-copy.test.js
+++ src/containers/YffConfirmationModal/company-email-copy.test.js
@@ -4,27 +4,29 @@ import CompanyEmailCopy from './company-email-copy'
 
 describe('Tester komponenten CompanyEmailCopy', () => {
   test('Komponenten rendrer som den skal', () => {
-    const { container } = render(<CompanyEmailCopy />)
+    const mockShowError = jest.fn()
+    const mockSetHasError = jest.fn()
+    const { container } = render(<CompanyEmailCopy showError={mockShowError} setHasError={mockSetHasError} />)
     expect(container).not.toBeEmptyDOMElement()
   })
 
   test('komponenten fungerer som forventet', async () => {
-    render(<CompanyEmailCopy />)
-    expect(await screen.queryByText(/send kopi på e-post/i)).not.toBeInTheDocument()
-    expect(screen.getByPlaceholderText(/send kopi på e-post/i)).toBeInTheDocument()
-    const epostfelt = screen.getByPlaceholderText(/send kopi på e-post/i)
-    userEvent.type(epostfelt, 'meg')
-    userEvent.tab()
-    expect(screen.getByText(/e-postadressen er ikke gyldig/i)).toBeInTheDocument()
-    userEvent.type(epostfelt, '@example.com')
-    expect(screen.getByText(/send kopi på e-post/i)).toBeInTheDocument()
+    const mockShowError = jest.fn()
+    const mockSetHasError = jest.fn()
+    render(<CompanyEmailCopy showError={mockShowError} setHasError={mockSetHasError} />)
+    expect(screen.getByPlaceholderText(/e-postadresse/i)).toBeInTheDocument()
+    const epostfelt = screen.getByPlaceholderText(/e-postadresse/i)
+    userEvent.type(epostfelt, 'meg@example.com')
+    expect(screen.getByText(/e-postadresse/i)).toBeInTheDocument()
     expect(epostfelt.value).toBe('meg@example.com')
   })
 
   test('komponenten kan slettes', async () => {
-    const { container } = render(<CompanyEmailCopy />)
+    const mockShowError = jest.fn()
+    const mockSetHasError = jest.fn()
+    const { container } = render(<CompanyEmailCopy showError={mockShowError} setHasError={mockSetHasError} />)
     expect(container).not.toBeEmptyDOMElement()
-    const sletteKnapp = screen.getByText(/slett/i)
+    const sletteKnapp = screen.getByLabelText(/slett kopimottager/i)
     userEvent.click(sletteKnapp)
     await waitForElementToBeRemoved(sletteKnapp)
     expect(container).toBeEmptyDOMElement()
