30c3ad1a6723c0b9bb8c49dc8318e172f752acec
jorgtho <jorgen.thorsnes@gmail.com>
2022-09-08 14:42:50 +0200

    added kontaktlarer info behind student name

--- src/pages/Class/basisgruppe.js
+++ src/pages/Class/basisgruppe.js
@@ -50,6 +50,7 @@ export function Basisgruppe ({ group, documents, conversations, notes }) {
                       <Link href={`/${ROUTES.students}/${student.username}`}>
                         {student.fullName}
                       </Link>
+                      {student.isContactTeacher ? <span title='Du er kontaktlærer for denne eleven'> <strong>(KL)</strong></span> : ''}
                     </Paragraph>
                   </div>
                 </td>
